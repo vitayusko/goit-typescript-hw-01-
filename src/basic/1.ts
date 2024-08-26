98819eef81365d9dbbb241c1df1d16f7d0a3e898
vitayusko <vitayusko@users.noreply.github.com>
2024-08-26 08:28:25 +0200

    1.ts -3.ts

--- src/basic/1.ts
+++ src/basic/1.ts
@@ -2,6 +2,6 @@ const age: number = 50;
 const username: string = "Max";
 const toggle: boolean = true;
 const empty: null = null;
-const callback: any = (a: any) => {
+const callback: (a: number) => number = (a: number) => {
   return 100 + a;
 };
--- src/generics/3.ts
+++ src/generics/3.ts
@@ -9,7 +9,7 @@ type AdditionFields = {
   age: number;
 };
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return Object.assign(objA, objB);
 }
 
